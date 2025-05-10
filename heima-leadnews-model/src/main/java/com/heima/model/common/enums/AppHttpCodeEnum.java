6741694e7ed91747a62fd5be348089fdbeb7499d
LuckyLeap <2754984034@qq.com>
2025-05-10 22:40:38 +0800

    黑马头条项目代码[Day 3] 修正

--- heima-leadnews-model/src/main/java/com/heima/model/common/enums/AppHttpCodeEnum.java
+++ heima-leadnews-model/src/main/java/com/heima/model/common/enums/AppHttpCodeEnum.java
@@ -28,7 +28,9 @@ public enum AppHttpCodeEnum {
     DATA_NOT_EXIST(1002,"数据不存在"),
     // 数据错误 3000~3500
     NO_OPERATOR_AUTH(3000,"无权限操作"),
-    NEED_ADMIND(3001,"需要管理员权限");
+    NEED_ADMIND(3001,"需要管理员权限"),
+    // 自媒体文章错误 3500~3600
+    MATERIASL_REFERENCE_FAIL(3500, "文章素材引用失败");
 
     final int code;
     final String errorMessage;
